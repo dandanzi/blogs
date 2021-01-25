64578ce0cb2058c322eb1446b1b6d30d5637771b
dandanzi <1586639761@qq.com>
2021-01-25 22:58:03 +0800

    '数据库配置'

--- blogs/blogs/settings.py
+++ blogs/blogs/settings.py
@@ -70,14 +70,20 @@ TEMPLATES = [
 WSGI_APPLICATION = 'blogs.wsgi.application'
 
 
+
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'django.db.backends.mysql',  #数据库引擎
+        'HOST': '127.0.0.1',   #数据库主机
+        'POST': '3306',   #数据库端口
+        'USER': 'dandanzi', #数据库用户名
+        'PASSWORD': '123456', #数据库用户密码
+        'NAME': 'dandanzi_blogs'  #数据库名字
+
+    },
 }
 
 
